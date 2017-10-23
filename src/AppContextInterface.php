498aaed27d72f4adadb3a379eb9cac6fdd9d6d60
Kael Shipman <kael.shipman@cfxtrading.com>
2017-10-23 01:10:24 -0500

    Progress on new model

--- src/AppContextInterface.php
+++ src/AppContextInterface.php
@@ -1,12 +1,12 @@
 <?php
 namespace CFX;
 
-interface AppContextInterface extends \KS\ConfigInterface {
+interface AppContextInterface extends \KS\BaseConfigInterface {
     public function getDisplayErrors();
     public function getErrorLevel();
-    public function serverRequestFromGlobals();
-    public function newRequest();
-    public function newServerRequest();
-    public function newResponse();
+    //public function serverRequestFromGlobals();
+    //public function newRequest();
+    //public function newServerRequest();
+    //public function newResponse();
 }
 
--- tests/AbstractDataContextTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class AbstractDataContextTest extends \PHPUnit\Framework\TestCase {
-    public function testIsValidJsonApiFactory() {
-    }
-}
-
