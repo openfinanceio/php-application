66849276a843d01074e137703715b1a4ea5a78eb
Kael Shipman <kael.shipman@cfxtrading.com>
2019-03-26 16:55:44 -0500

    Upgraded Guzzle dev dependency from 5 to 6 and improved mock http client

--- src/Test/HttpClient.php
+++ src/Test/HttpClient.php
@@ -5,7 +5,13 @@ class HttpClient extends \GuzzleHttp\Client {
     protected $nextResponse = [];
     protected $requestTrace = [];
 
-    public function setNextResponse(\GuzzleHttp\Message\ResponseInterface $r) {
+    public function setNextResponse($r) {
+        if (
+            !($r instanceof \Psr\Http\Message\ResponseInterface) &&
+            !($r instanceof \RuntimeException)
+        ) {
+            throw new \TypeError("First argument must be a \Psr\Http\Message\ResponseInterface or some kind of exception");
+        }
         $this->nextResponse[] = $r;
         return $this;
     }
@@ -16,11 +22,16 @@ class HttpClient extends \GuzzleHttp\Client {
         return $this->requestTrace[$i-1];
     }
 
-    public function send(\GuzzleHttp\Message\RequestInterface $r) {
+    public function send(\Psr\Http\Message\RequestInterface $request, array $options = []) {
         if (count($this->nextResponse) == 0) throw new \RuntimeException("This is a test HTTP Client that does not make real HTTP calls. You must set the response for the request you're about to execute by using the `setNextResponse(\GuzzleHttp\Message\ResponseInterface \$r)` method.");
 
-        $this->requestTrace[] = $r;
+        $this->requestTrace[] = $request;
         $res = array_shift($this->nextResponse);
+
+        if ($res instanceof \Exception) {
+            throw $res;
+        }
+
         return $res;
     }
 }
---
+++ tests/Test/HttpClientTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace CFX\Test;
+
+class HttpClientTest extends \PHPUnit\Framework\TestCase
+{
+    protected $httpClient;
+
+    public function setUp()
+    {
+        $this->httpClient = new HttpClient();
+    }
+
+    public function testSetResponse()
+    {
+        $r = new \GuzzleHttp\Psr7\Response();
+        $this->httpClient->setNextResponse($r);
+        $this->assertSame($r, $this->httpClient->send(new \GuzzleHttp\Psr7\Request("GET", "/test")));
+    }
+
+    public function testThrowsExceptionIfNoResponseSet()
+    {
+        try {
+            $this->httpClient->send(new \GuzzleHttp\Psr7\Request("GET", "/test"));
+            $this->fail("Should have thrown an exception");
+        } catch(\RuntimeException $e) {
+            $this->assertEquals(
+                "This is a test HTTP Client that does not make real HTTP calls. You must set the response ".
+                "for the request you're about to execute by using the `setNextResponse(\GuzzleHttp\Message".
+                "\ResponseInterface \$r)` method.",
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function testThrowsExceptionIfGivenAnExceptionAsNextResponse()
+    {
+        $req = new \GuzzleHttp\Psr7\Request("GET", "/test");
+        $res = new \GuzzleHttp\Exception\ServerException("Something happened", $req);
+        $this->httpClient->setNextResponse($res);
+        try {
+            $this->httpClient->send($req);
+            $this->fail("Should have thrown an exception");
+        } catch (\GuzzleHttp\Exception\ServerException $e) {
+            $this->assertSame($res, $e);
+        }
+    }
+
+    public function testFIFO()
+    {
+        $r1 = new \GuzzleHttp\Psr7\Response();
+        $r2 = new \GuzzleHttp\Psr7\Response();
+        $this->httpClient
+            ->setNextResponse($r1)
+            ->setNextResponse($r2);
+
+        $this->assertSame($r1, $this->httpClient->send(new \GuzzleHttp\Psr7\Request("GET", "/test")));
+        $this->assertSame($r2, $this->httpClient->send(new \GuzzleHttp\Psr7\Request("GET", "/test")));
+    }
+}
