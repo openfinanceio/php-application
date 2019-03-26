60d096952c79aefd2d552ae02c27e4f9099a496e
Kael Shipman <kael.shipman@cfxtrading.com>
2019-03-26 10:09:46 -0500

    Changed mock HttpClient to FIFO instead of FILO

--- src/Test/HttpClient.php
+++ src/Test/HttpClient.php
@@ -20,7 +20,7 @@ class HttpClient extends \GuzzleHttp\Client {
         if (count($this->nextResponse) == 0) throw new \RuntimeException("This is a test HTTP Client that does not make real HTTP calls. You must set the response for the request you're about to execute by using the `setNextResponse(\GuzzleHttp\Message\ResponseInterface \$r)` method.");
 
         $this->requestTrace[] = $r;
-        $res = array_pop($this->nextResponse);
+        $res = array_shift($this->nextResponse);
         return $res;
     }
 }
