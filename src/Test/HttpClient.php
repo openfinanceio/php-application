862a9dac71c32dbfa2d0f6b29a602db55a21a239
Kael Shipman <kael.shipman@cfxtrading.com>
2019-07-01 12:54:32 -0500

    Minor improvement to mock http client

--- src/Test/HttpClient.php
+++ src/Test/HttpClient.php
@@ -38,7 +38,7 @@ class HttpClient extends \GuzzleHttp\Client {
     }
 
     protected function transferStub(\Psr\Http\Message\RequestInterface $request, array $options = []) {
-        if (count($this->nextResponse) == 0) throw new \RuntimeException("This is a test HTTP Client that does not make real HTTP calls. You must set the response for the request you're about to execute by using the `setNextResponse(\GuzzleHttp\Message\ResponseInterface \$r)` method.");
+        if (count($this->nextResponse) == 0) throw new \RuntimeException("This is a test HTTP Client that does not make real HTTP calls. You must set the response for the request you're about to execute by using the `setNextResponse(\GuzzleHttp\Message\ResponseInterface \$r)` method.\n\nRequest details: {$request->getMethod()} {$request->getUri()}");
 
         $request = $this->applyOptionsStub($request, $options);
 
